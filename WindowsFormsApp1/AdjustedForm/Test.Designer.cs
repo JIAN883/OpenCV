27d58bf96a6f616b5bcbd18be94c090fea5f6288
JIAN883 <10624221@gmail.com>
2021-06-12 20:57:11 +0800

    修改測試區

--- WindowsFormsApp1/AdjustedForm/Test.Designer.cs
+++ WindowsFormsApp1/AdjustedForm/Test.Designer.cs
@@ -29,26 +29,50 @@ namespace WindowsFormsApp1.AdjustedForm
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(171, 424);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            // 
             // trackBar1
             // 
-            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.trackBar1.Location = new System.Drawing.Point(0, 425);
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar1.Location = new System.Drawing.Point(12, 467);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(832, 70);
-            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Size = new System.Drawing.Size(812, 56);
+            this.trackBar1.TabIndex = 2;
             this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(411, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "輸入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 495);
+            this.ClientSize = new System.Drawing.Size(859, 535);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Test";
             this.Text = "Test";
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
@@ -59,6 +83,8 @@ namespace WindowsFormsApp1.AdjustedForm
 
         #endregion
 
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
     }
 }
