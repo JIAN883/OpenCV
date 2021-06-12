b69c5985a402cbd0ae851434a251b28cc677b675
d800853 <d800853@gmail.com>
2021-06-12 17:33:33 +0800

    C++    LaplicianFilter    getUnsharpInformation    HighboostFilter

--- imgFunc/imgFunc.cpp
+++ imgFunc/imgFunc.cpp
@@ -4,7 +4,7 @@
 #include "imgFunc.h"
 #include <opencv2/opencv.hpp>
 using namespace cv;
-
+Mat global_temp_mat;//用來當函式呼叫之間的媒介，每次用完記得release
 // This is an example of an exported function.
 IMGFUNC_API void Blur(unsigned char* imageBuffer, int width, int height, float value)
 {
@@ -52,7 +52,7 @@ IMGFUNC_API void MaxOrMinFilter(unsigned char* imageBuffer, int width, int heigh
 		for (int ch = 0; ch < channelNum; ch++) {
 			for (int row = 0; row < src.rows; row++) {
 				for (int column = 0; column < src.cols; column++) {
-					//跑kernel 判斷maxValue或minValue
+					//跑kernel 判斷Channel是多少 也判斷maxValue或minValue
 					for (int tempR = row- KernelSize/2; tempR < (row+(KernelSize+1)/2); tempR++) {
 						for (int tempC = column - KernelSize / 2; tempC < (column + (KernelSize + 1) / 2); tempC++) {
 							if (tempR < 0 || tempC < 0 || tempR >= src.rows|| tempC >= src.cols) continue;
@@ -97,6 +97,52 @@ IMGFUNC_API void MaxOrMinFilter(unsigned char* imageBuffer, int width, int heigh
 		}
 		src = dst.clone();
 	}
-	
 }
+
+//CH3_銳化濾波器(Sharp/Laplician filter)
+	//isAddOriImage：true->有再加原圖,false->沒有加原圖(純邊緣資訊)
+	//沒有開周圍4格，目前統一周圍8格
+IMGFUNC_API void LaplicianFilter(unsigned char* imageBuffer, int width, int height, bool isAddOriImage)
+{
+	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
+	if (!src.empty()) {
+		Mat mask;
+		if(isAddOriImage)
+			mask = (Mat_<double>(3, 3) << -1, -1, -1, -1, 9, -1, -1, -1, -1);
+		else mask = (Mat_<double>(3, 3) << -1, -1, -1, -1, 8, -1, -1, -1, -1);
+		filter2D(src, src, src.depth(), mask);
+	}
+}
+
+//CH3_取得鈍化圖形資訊(UnsharpInformation)
+	//
+IMGFUNC_API void getUnsharpInformation(unsigned char* imageBuffer, int width, int height)
+{
+	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
+	if (!src.empty()) {
+		Mat blu, unsharp_mask;
+		//Prepare and apply the Laplacian filter
+		Mat mask = (Mat_<double>(3, 3) << 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9);
+		filter2D(src, blu, src.depth(), mask);
+		unsharp_mask = src - blu; //取出特徵(unsharp mask)
+		src = unsharp_mask.clone();
+	}
+}
+
+//CH3_高增幅濾波器(Highboost filter)
+	//k：鈍化倍數 float(>=0)
+IMGFUNC_API void HighboostFilter(unsigned char* imageBuffer, int width, int height,float k)
+{
+	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
+	if (!src.empty()) {
+		Mat blu, unsharp_mask, sharp_mat;
+		//Prepare and apply the Laplacian filter
+		Mat mask = (Mat_<double>(3, 3) << 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9);
+		filter2D(src, blu, src.depth(), mask);
+		unsharp_mask = src - blu; //取出特徵(unsharp mask)
+		sharp_mat = src + k * unsharp_mask;
+		src = sharp_mat.clone();
+	}
+}
+
 
