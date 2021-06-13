53993d342646aae497bc509beb47a2c9bab7ede5
d800853 <d800853@gmail.com>
2021-06-13 17:59:14 +0800

    use global value

--- imgFunc/imgFunc.cpp
+++ imgFunc/imgFunc.cpp
@@ -5,7 +5,8 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 #define PI 3.14159
-Mat global_temp_mat;//用來當函式呼叫之間的媒介，每次用完記得release
+#define global_size 5
+Mat global_temp_mat[global_size];//用來當函式呼叫之間的媒介，每次用完記得release
 
 void copyContent(Mat src, Mat dst) {
 	int channelNum = src.channels();
@@ -56,7 +57,7 @@ IMGFUNC_API void MedianFilter(unsigned char* imageBuffer, int width, int height,
 }
 
 //CH3_最大值濾波器 mode：0->maxFilter,!=0->minFilter   KernelSize 濾波器kernel = KernelSize * KernelSize (可接受偶數*偶數) 
-IMGFUNC_API void MaxOrMinFilter(unsigned char* imageBuffer, int width, int height,int mode, float KernelSize)
+IMGFUNC_API void MaxOrMinFilter(unsigned char* imageBuffer, int width, int height,int mode, int KernelSize,void* &dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -110,7 +111,9 @@ IMGFUNC_API void MaxOrMinFilter(unsigned char* imageBuffer, int width, int heigh
 				}
 			}
 		}
-		copyContent(dst, src);
+		global_temp_mat[0]=dst.clone();
+		dstBuffer = global_temp_mat[0].data;
+		//copyContent(dst, src);
 	}
 }
 
@@ -131,7 +134,7 @@ IMGFUNC_API void LaplicianFilter(unsigned char* imageBuffer, int width, int heig
 
 //CH3_取得鈍化圖形資訊(UnsharpInformation)
 	//
-IMGFUNC_API void getUnsharpInformation(unsigned char* imageBuffer, int width, int height)
+IMGFUNC_API void getUnsharpInformation(unsigned char* imageBuffer, int width, int height, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -140,13 +143,15 @@ IMGFUNC_API void getUnsharpInformation(unsigned char* imageBuffer, int width, in
 		Mat mask = (Mat_<double>(3, 3) << 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9, 1. / 9);
 		filter2D(src, blu, src.depth(), mask);
 		unsharp_mask = src - blu; //取出特徵(unsharp mask)
-		copyContent(unsharp_mask, src);
+		global_temp_mat[0] = unsharp_mask.clone();
+		dstBuffer = global_temp_mat[0].data;
+		//copyContent(unsharp_mask, src);
 	}
 }
 
 //CH3_高增幅濾波器(Highboost filter)
 	//k：鈍化倍數 float(>=0)
-IMGFUNC_API void HighboostFilter(unsigned char* imageBuffer, int width, int height,float k)
+IMGFUNC_API void HighboostFilter(unsigned char* imageBuffer, int width, int height,float k, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -156,7 +161,9 @@ IMGFUNC_API void HighboostFilter(unsigned char* imageBuffer, int width, int heig
 		filter2D(src, blu, src.depth(), mask);
 		unsharp_mask = src - blu; //取出特徵(unsharp mask)
 		sharp_mat = src + k * unsharp_mask;
-		copyContent(sharp_mat, src);
+		global_temp_mat[0] = sharp_mat.clone();
+		dstBuffer = global_temp_mat[0].data;
+		//copyContent(sharp_mat, src);
 	}
 }
 
@@ -164,7 +171,7 @@ IMGFUNC_API void HighboostFilter(unsigned char* imageBuffer, int width, int heig
 	//目前mask矩陣大小固定
 	//isHorizontal：true->水平,false->垂直
 	//isAddOriImage：true->有再加原圖,false->沒有加原圖(純水平或垂直強度資訊)
-IMGFUNC_API void horizontalIntensityFilter(unsigned char* imageBuffer, int width, int height,bool isHorizontal,bool isAddOriImage)
+IMGFUNC_API void horizontalIntensityFilter(unsigned char* imageBuffer, int width, int height,bool isHorizontal,bool isAddOriImage, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -177,7 +184,9 @@ IMGFUNC_API void horizontalIntensityFilter(unsigned char* imageBuffer, int width
 		if (isAddOriImage)
 			mask.at<double>(1, 1) = 1;
 		filter2D(src, dst, src.depth(), mask);
-		copyContent(dst, src);
+		global_temp_mat[0] = dst.clone();
+		dstBuffer = global_temp_mat[0].data;
+		//copyContent(dst, src);
 	}
 }
 
@@ -203,7 +212,7 @@ IMGFUNC_API void negative(unsigned char* imageBuffer, int width, int height)
 
 //CH3_亮度調整1(Log)
 	//c ：亮度倍率(>1,可預設2,float)
-IMGFUNC_API void brightProcessing_log(unsigned char* imageBuffer, int width, int height,float c)
+IMGFUNC_API void brightProcessing_log(unsigned char* imageBuffer, int width, int height,float c, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -211,14 +220,16 @@ IMGFUNC_API void brightProcessing_log(unsigned char* imageBuffer, int width, int
 		src.convertTo(log_dst, CV_32F, 1.f / 255.f);
 		cv::log(log_dst + 1, log_dst);
 		log_dst = c * log_dst / log(2.0);
-		copyContent(log_dst, src);
+		global_temp_mat[0] = log_dst.clone();
+		dstBuffer = global_temp_mat[0].data;
+		//copyContent(log_dst, src);
 	}
 }
 
 //CH3_亮度調整2(Power conversion)
 	//c ：亮度倍率(>=1,可預設1,float)
 	//gamma：指數參數(=1->不變, >1->變暗, <1->變亮,可預設0.5)
-IMGFUNC_API void brightProcessing_power(unsigned char* imageBuffer, int width, int height, float c,float gamma)
+IMGFUNC_API void brightProcessing_power(unsigned char* imageBuffer, int width, int height, float c,float gamma, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -226,6 +237,8 @@ IMGFUNC_API void brightProcessing_power(unsigned char* imageBuffer, int width, i
 		src.convertTo(log_dst, CV_32F, 1.f / 255.f);
 		cv::pow(log_dst, gamma, log_dst);
 		log_dst = c * log_dst ;
+		global_temp_mat[0] = log_dst.clone();
+		dstBuffer = global_temp_mat[0].data;
 		copyContent(log_dst, src);
 	}
 }
@@ -248,7 +261,7 @@ IMGFUNC_API void bitPlaneSlicing(unsigned char* imageBuffer, int width, int heig
 
 //CH3_直方圖(Histogram Processing)
 	//
-IMGFUNC_API void HistogramProcessing(unsigned char* imageBuffer, unsigned char* imageBufferB, unsigned char* imageBufferG, unsigned char* imageBufferR,int width, int height, int bit)
+IMGFUNC_API void HistogramProcessing(unsigned char* imageBuffer,int width, int height, int bit, void*& dstBufferB, void*& dstBufferG, void*& dstBufferR)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -264,13 +277,34 @@ IMGFUNC_API void HistogramProcessing(unsigned char* imageBuffer, unsigned char* 
 		normalize(hist_b, hist_b, 0, src.rows, NORM_MINMAX);//正規化(以最高的直方當最高點) //NORM_MINMAX：選擇的正規化方法 389：最高 0：最低
 		normalize(hist_g, hist_g, 0, src.rows, NORM_MINMAX);
 		normalize(hist_r, hist_r, 0, src.rows, NORM_MINMAX);
-		//要傳hist_b, hist_g, hist_r到c#
+		// Draw the histogram
+		int hist_w = 512, hist_h = 400, bin_w = (double)hist_w / histSize + 0.5;
+		//hist_b
+		global_temp_mat[0]=Mat(hist_h, hist_w, CV_8U, Scalar(255)); //圖高 圖寬 單位  Scalar(255)：會是全白色(全部255)
+		for (int i = 0; i < histSize; i++) {
+			line(global_temp_mat[0], Point(bin_w * i, hist_h), Point(bin_w * i, hist_h - hist_b.at<float>(i)), Scalar(0)); //畫線 *圖是左上為原點：由底往上畫
+		}
+		//hist_g
+		global_temp_mat[1] = Mat(hist_h, hist_w, CV_8U, Scalar(255)); //圖高 圖寬 單位  Scalar(255)：會是全白色(全部255)
+		for (int i = 0; i < histSize; i++) {
+			line(global_temp_mat[1], Point(bin_w * i, hist_h), Point(bin_w * i, hist_h - hist_g.at<float>(i)), Scalar(0)); //畫線 *圖是左上為原點：由底往上畫
+		}
+		//hist_r
+		global_temp_mat[2] = Mat(hist_h, hist_w, CV_8U, Scalar(255)); //圖高 圖寬 單位  Scalar(255)：會是全白色(全部255)
+		for (int i = 0; i < histSize; i++) {
+			line(global_temp_mat[2], Point(bin_w * i, hist_h), Point(bin_w * i, hist_h - hist_r.at<float>(i)), Scalar(0)); //畫線 *圖是左上為原點：由底往上畫
+		}
+		//return to c#
+		dstBufferB = global_temp_mat[0].data;
+		dstBufferG = global_temp_mat[1].data;
+		dstBufferR = global_temp_mat[2].data;
+		
 	}
 }
 
 //CH3_等化直方圖處理(equalizeHist)
 	//mode (int,1->RGB,2->HSV)
-IMGFUNC_API void equalizeHist(unsigned char* imageBuffer, int width, int height, int mode)
+IMGFUNC_API void equalizeHist(unsigned char* imageBuffer, int width, int height, int mode, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -290,7 +324,10 @@ IMGFUNC_API void equalizeHist(unsigned char* imageBuffer, int width, int height,
 			merge(Channels, 3, dst);
 			cvtColor(dst, dst, COLOR_HSV2BGR);
 		}
-		copyContent(dst, src);
+		//return to c#
+		global_temp_mat[0] = dst.clone();
+		dstBuffer = global_temp_mat[0].data;
+		//copyContent(dst, src);
 	}
 }
 
@@ -298,7 +335,7 @@ IMGFUNC_API void equalizeHist(unsigned char* imageBuffer, int width, int height,
 	//xtimes ：x軸放大倍數(double,>0.1)
 	//ytimes ：x軸放大倍數(double,>0.1)
 	//isfullsize ：記憶體大小是否隨著放大縮小而改變 (true：隨著新圖改變大小，false：不改變大小，可直接設成true)
-IMGFUNC_API void changeImageSize(unsigned char* imageBuffer, int width, int height, double xtimes, double ytimes, bool isfullsize)
+IMGFUNC_API void changeImageSize(unsigned char* imageBuffer, int width, int height, double xtimes, double ytimes, bool isfullsize, int*& dst_width, int*& dst_height, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -313,13 +350,18 @@ IMGFUNC_API void changeImageSize(unsigned char* imageBuffer, int width, int heig
 		else {
 			warpAffine(src, dst, M, src.size()); // call opencv's affine transform
 		}
-		//要傳dst到c#
+		//return to c#
+		*dst_width = (int)width * xtimes;
+		*dst_height= (int)height * ytimes;
+		global_temp_mat[0] = dst.clone();
+		dstBuffer = global_temp_mat[0].data;
+		
 	}
 }
 
 //CH2_旋轉(Rotate)
 	//angle ：順時針旋轉angle度(double)
-IMGFUNC_API void Rotate(unsigned char* imageBuffer, int width, int height, double angle)
+IMGFUNC_API void Rotate(unsigned char* imageBuffer, int width, int height, double angle, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -327,16 +369,16 @@ IMGFUNC_API void Rotate(unsigned char* imageBuffer, int width, int height, doubl
 		double rad = PI * angle / 180.0; // degree to radian
 		Mat M = (Mat_<double>(2, 3) << cos(rad), -sin(rad), 0, sin(rad), cos(rad), 0); // prepare matrix 2->colunm數 3->row數，左上到左下中上到中下
 		warpAffine(src, dst, M, src.size()); // call opencv's affine transform
-
-		//要傳dst到c#
-		//或者試這個 copyContent(dst, src);
+		//return to c#
+		global_temp_mat[0] = dst.clone();
+		dstBuffer = global_temp_mat[0].data;
 	}
 }
 
 //CH2_剪形(Shear)
 	//xshear ：水平剪形程度 (double)
 	//yshear ：垂直剪形程度 (double)
-IMGFUNC_API void Shear(unsigned char* imageBuffer, int width, int height, double xshear, double yshear)
+IMGFUNC_API void Shear(unsigned char* imageBuffer, int width, int height, double xshear, double yshear, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -344,15 +386,16 @@ IMGFUNC_API void Shear(unsigned char* imageBuffer, int width, int height, double
 		Mat M = (Mat_<double>(2, 3) << 1, xshear, 0, yshear, 1, 0); 
 		warpAffine(src, dst, M, src.size());
 
-		//要傳dst到c#
-		//或者試這個 copyContent(dst, src);
+		//return to c#
+		global_temp_mat[0] = dst.clone();
+		dstBuffer = global_temp_mat[0].data;
 	}
 }
 
 //CH2_鏡射(Reflect)
 	//isReflectAboutXaxis：true->上下鏡射 false->不上下鏡射
 	//isReflectAboutYaxis：true->左右鏡射 false->不左右鏡射
-IMGFUNC_API void Reflect(unsigned char* imageBuffer, int width, int height, bool isReflectAboutXaxis, bool isReflectAboutYaxis)
+IMGFUNC_API void Reflect(unsigned char* imageBuffer, int width, int height, bool isReflectAboutXaxis, bool isReflectAboutYaxis, void*& dstBuffer)
 {
 	Mat src = Mat(height, width, CV_8UC3, imageBuffer);
 	if (!src.empty()) {
@@ -368,8 +411,44 @@ IMGFUNC_API void Reflect(unsigned char* imageBuffer, int width, int height, bool
 			M = (Mat_<double>(2, 3) << 1, 0, 0, 0, 1, 0); //維持不變
 		warpAffine(src, dst, M, src.size());
 
-		//要傳dst到c#
-		//或者試這個 copyContent(dst, src);
+		//return to c#
+		global_temp_mat[0] = dst.clone();
+		dstBuffer = global_temp_mat[0].data;
 	}
 }
-
+//頻率域
+void swapFreq(Mat& F)
+{
+	int cx = F.cols / 2;
+	int cy = F.rows / 2;
+	Mat q0(F, Rect(0, 0, cx, cy)); // Top-Left
+	Mat q1(F, Rect(cx, 0, cx, cy)); // Top-Right
+	Mat q2(F, Rect(0, cy, cx, cy)); // Bottom-Left
+	Mat q3(F, Rect(cx, cy, cx, cy)); // Bottom-Right
+	//swap quadrants
+	Mat tmp;
+	q0.copyTo(tmp);
+	q3.copyTo(q0);
+	tmp.copyTo(q3);
+	q1.copyTo(tmp);
+	q2.copyTo(q1);
+	tmp.copyTo(q2);
+}
+// DFT transform
+Mat myDFT(Mat& f)
+{
+	Mat F; //frequency data
+	Mat padded; //Padded image
+	//Pad image to optimal DFT size with 0s
+	int m = getOptimalDFTSize(f.rows);
+	int n = getOptimalDFTSize(f.cols);
+	copyMakeBorder(f, padded, 0, m - f.rows, 0, n - f.cols,
+		BORDER_CONSTANT, Scalar::all(0));
+	// allocate memory for storing frequency data and doing DFT
+	Mat planes[] = { Mat_<float>(padded),
+	Mat(padded.size(), CV_32F, 0.0f) };
+	merge(planes, 2, F);
+	dft(F, F);
+	swapFreq(F);
+	return F;
+}
